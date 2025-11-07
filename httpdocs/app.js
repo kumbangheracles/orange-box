eda320945036588a2c7a595b070e2aff001ac102
Kumbang heracles <kumbangheracles@gmail.com>
2025-11-07 21:11:32 +0700

    adjust app.js

--- httpdocs/app.js
+++ httpdocs/app.js
@@ -2,15 +2,14 @@ import createServer from "next/dist/server/next";
 import next from "next";
 
 const port = 3000;
-const dev = process.env.NODE_ENV !== "production";
+const dev = false;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
     handle(req, res);
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+  }).listen(port, () => {
+    console.log(`> Server running on port ${port}`);
   });
 });
